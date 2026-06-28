e9af0a53dcd40808cf8f76310d4473e008edbf14
krsr08 <ravisankarreddykasu@gmail.com>
2026-06-28 09:38:53 +0530

    Fix settings screen compile import

--- android/app/src/main/java/com/soulmatch/app/ui/screens/settings/SettingsScreen.kt
+++ android/app/src/main/java/com/soulmatch/app/ui/screens/settings/SettingsScreen.kt
@@ -1,6 +1,7 @@
 package com.soulmatch.app.ui.screens.settings
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
